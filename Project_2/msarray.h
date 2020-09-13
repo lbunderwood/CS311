bd877f833ac4acf168ff82edad7f73cd1348ab14
luke.b.underwood@gmail.com <luke.b.underwood@gmail.com>
2020-09-13 12:46:38 -0800

    msarray framework

---
+++ Project_2/msarray.h
@@ -0,0 +1,60 @@
+// Luke Underwood
+// 9/13/2020
+// msarray.h
+// CS311 - Data Structures and Algorithms
+// Implementation of class MSArray
+
+#ifndef MSARRAY_H
+#define MSARRAY_H
+
+// Class MSArray
+// A moderately smart array template class
+// Invariants : 
+// Restrictions on Types : 
+template <typename T>
+class MSArray
+{
+public:
+
+    // Default Constructor
+    MSArray();
+
+    //////////////////////////////////////////////
+    // Big Five Constuctors, written explicitly //
+    //////////////////////////////////////////////
+
+    // Copy constuctor
+    MSArray(const MSArray & old)
+    {
+
+    }
+
+    // Move constructor
+    MSArray(MSArray && old)
+    {
+
+    }
+
+    // Copy Assignment Operator
+    MSArray operator=(const MSArray & old)
+    {
+
+    }
+
+    // Move Assignment Operator
+    MSArray operator=(MSArray && old)
+    {
+
+    }
+
+    // Destructor
+    ~MSArray()
+    {
+
+    }
+
+private:
+
+};
+
+#endif // MSARRAY_H
