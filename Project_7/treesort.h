92440d347e2c188adb452be7b581cf18b58d8a0e
lbunderwood <luke.b.underwood@gmail.com>
2020-11-14 12:25:46 -0900

    treesort.h - passes tests, comments in progress

--- Project_7/treesort.h
+++ Project_7/treesort.h
@@ -20,24 +20,22 @@
 // For std::iterator_traits
 #include <memory>
 // For std::unique_ptr, std::make_unique
-#include <deque>
-// For std::deque
-
-// *******************************************************
-// * YOU MIGHT WANT TO GET RID OF THE FOLLOWING INCLUDES *
-// *******************************************************
-
-#include <algorithm>
-// For std::move
-#include <vector>
-// For std::vector
-#include <iterator>
-// For std::distance
 
 // template class BSNode
-// 
-//
-//
+//  Binary Search Tree Node - contains a piece of information and pointers to left and right child nodes
+//  Nodes should only be reffered to via smart pointers
+//  If a child node is nullptr, it does not exist
+//  There are two helper functions for this class that have been made friends - insert and inorder
+//  The only valid constructor is one-parameter, given the data it is to store
+//  This constructor should not be called directly, but via make_unique
+// Class Invariants :
+//     All values stored in the left subtree (the node pointed to by left_ and all of it's decendants)
+//      must be < the value stored in this node.
+//     All values stored in the right subtree (see above definition, but with right_) must be >= the value
+//      stored in this node.
+// Restrictions on Types : 
+//     None
+//     This is not true for helper functions
 template <typename T>
 class BSNode
 {
@@ -52,6 +50,8 @@ public:
 
 ////////// BSNode : Constructors & Destructor
 public:
+
+    BSNode(const value_type& value) : data_(value), right_(nullptr), left_(nullptr) {}
 
     // Default Constructor
     // Deleted, so we don't have to require that value_type have a default constructor
@@ -72,8 +72,10 @@ public:
 public:
 
     // see documentation below
-    friend void insert(const BSNode<value_type>& head, const value_type& data);
-    friend std::deque<value_type> inorder(const BSNode<value_type>& head);
+    template<typename V>
+    friend void insert(const std::unique_ptr<BSNode<V>>& root, const V& data);
+    template<typename V>
+    friend std::deque<V> inorder(const std::unique_ptr<BSNode<V>>& root);
 
 ////////// BSNode : Member Variables
 public:
@@ -94,29 +96,59 @@ public:
 // insert
 // Inserts an item into a BSNode tree given the root pointer and the item
 // Pre:
-//     ???
+//     root must either be nullptr or a pointer to the root of a valid BSNode tree
+//     data must be an lvalue
 // Requirements on Types:
-//     ???
+//     V must be comparable via <
 // Exception safety guarantee:
-//     ???
-template<typename value_type>
-void insert(const BSNode::ptr_type& head, const value_type& data)
+//     Strong guaruntee - final operation is the only one that changes data
+template<typename V>
+void insert(std::unique_ptr<BSNode<V>>& root, const V& data)
 {
-    // TODO : Write this
+    // if this node doesn't exist
+    if(!root)
+    {
+        // create the node
+        root = std::make_unique<BSNode<V>>(data);
+    }
+    // if the data is < the current node's data
+    else if(data < root->data_)
+    {
+        // recurse with left subtree
+        insert(root->left_, data);
+    }
+    // data >= root->data_ (since this is the only other possibility)
+    else
+    {
+        // recurse with right subtree
+        insert(root->right_, data);
+    }
 }
 
 // inorder
-// Returns an inorder traversal of a BSNode tree given the root pointer
+// Returns an inorder traversal of a BSNode tree given the root pointer and an iterator to write to
 // Pre:
 //     ???
 // Requirements on Types:
-//     ???
+//     The type reffered to by iter must have a copy assignment operator
+//     Iter must have a prefix operator++
 // Exception safety guarantee:
-//     ???
-template<typename value_type>
-std::deque<value_type> inorder(const BSNode::ptr_type& head)
+//     Basic Guaruntee - data is changed throughout the process, so the strong guaruntee cannot be offered
+template<typename Iter>
+void inorder(std::unique_ptr<BSNode<typename std::iterator_traits<Iter>::value_type>>& root, Iter& iter)
 {
-    return std::deque<value_type>();
+    // if this node does not exist, get us out of here
+    if(!root) return;
+    
+    // traverse left subtree
+    inorder(root->left_, iter);
+
+    // write current node's data to position of iterator
+    *iter = root->data_;
+    ++iter;
+
+    // traverse right subtree
+    inorder(root->right_, iter);
 }
 
 // treesort
@@ -132,8 +164,18 @@ void treesort(FDIter first, FDIter last)
 {
     // Value is the type that FDIter points to
     using Value = typename std::iterator_traits<FDIter>::value_type;
-    BSNode::ptr_type<Value> root = std::make_unique<Value>;;
-    
+
+    // create root node
+    std::unique_ptr<BSNode<Value>> root = nullptr;
+
+    // create binary search tree
+    for(FDIter i = first; i != last; ++i)
+    {
+        insert(root, *i);
+    }
+
+    // write inorder traversal to the given range
+    inorder(root, first);
 }
 
 
