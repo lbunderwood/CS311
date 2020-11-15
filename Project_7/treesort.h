93444ffb5cbbdf1ad3522c04a89b61338cb3cf73
lbunderwood <luke.b.underwood@gmail.com>
2020-11-14 16:36:36 -0900

    treesort.h - added some comments

--- Project_7/treesort.h
+++ Project_7/treesort.h
@@ -21,10 +21,15 @@
 #include <memory>
 // For std::unique_ptr, std::make_unique
 
+
+
+
 // template class BSNode
 //  Binary Search Tree Node - contains a piece of information and pointers to left and right child nodes
 //  Nodes should only be reffered to via smart pointers
 //  If a child node is nullptr, it does not exist
+//  Multiple nodes with the same value are allowed. If a node is inserted with the same value as a
+//   pre-existing node, it will be inserted into the leftmost position in the right subtree of that node.
 //  There are two helper functions for this class that have been made friends - insert and inorder
 //  The only valid constructor is one-parameter, given the data it is to store
 //  This constructor should not be called directly, but via make_unique
@@ -51,6 +56,12 @@ public:
 ////////// BSNode : Constructors & Destructor
 public:
 
+    // One-parameter constructor
+    //  Takes a value to store in data_
+    // Pre : 
+    //     value must be a valid lvalue of the type being templated
+    // Exception Safey Guaruntee : 
+    //     Strong Guaruntee - value is reference to const and cannot be modified
     BSNode(const value_type& value) : data_(value), right_(nullptr), left_(nullptr) {}
 
     // Default Constructor
@@ -73,12 +84,13 @@ public:
 
     // see documentation below
     template<typename V>
-    friend void insert(const std::unique_ptr<BSNode<V>>& root, const V& data);
-    template<typename V>
-    friend std::deque<V> inorder(const std::unique_ptr<BSNode<V>>& root);
+    friend void insert(std::unique_ptr<BSNode<V>>& root, const V& data);
+    template<typename Iter>
+    friend void inorder(std::unique_ptr<BSNode<
+        typename std::iterator_traits<Iter>::value_type>>& root, Iter& iter);
 
 ////////// BSNode : Member Variables
-public:
+private:
 
     // the piece of information stored in this node
     value_type data_;
@@ -88,6 +100,9 @@ public:
     ptr_type right_;
 
 };
+
+
+
 
 ///////////////////////////////////////////////////////
 /////////   BSNode Helper Functions
@@ -102,6 +117,8 @@ public:
 //     V must be comparable via <
 // Exception safety guarantee:
 //     Strong guaruntee - final operation is the only one that changes data
+//     Exception Neuteral
+//     Does not throw except via operations and functions called
 template<typename V>
 void insert(std::unique_ptr<BSNode<V>>& root, const V& data)
 {
@@ -138,6 +155,8 @@ void insert(std::unique_ptr<BSNode<V>>& root, const V& data)
 //     Iter must have a prefix operator++
 // Exception safety guarantee:
 //     Basic Guaruntee - data is changed throughout the process, so the strong guaruntee cannot be offered
+//     Exception Neuteral
+//     Does not throw except via operations and functions called
 template<typename Iter>
 void inorder(std::unique_ptr<BSNode<typename std::iterator_traits<Iter>::value_type>>& root, Iter& iter)
 {
@@ -155,9 +174,16 @@ void inorder(std::unique_ptr<BSNode<typename std::iterator_traits<Iter>::value_t
     inorder(root->right_, iter);
 }
 
+
+
+
+//////////////////////////////////////////////////////////////
+////////    Treesort Algorithm
+//////////////////////////////////////////////////////////////
+
 // treesort
 // Sort a given range using Treesort.
-// function prototype and the "using Value = ..." statement written by Glenn Chappell
+// function prototype and the line "using Value = ..." written by Glenn Chappell
 // Pre:
 //     first and last must be iterators that refer to the beginning and 
 //      one past the end of a container, respectively
@@ -166,6 +192,8 @@ void inorder(std::unique_ptr<BSNode<typename std::iterator_traits<Iter>::value_t
 //      everything required by insert and inorder
 // Exception safety guarantee:
 //     Basic Guaruntee - inorder can only offer the basic guaruntee
+//     Exception Neuteral
+//     Does not throw except via operations and functions called
 template<typename FDIter>
 void treesort(FDIter first, FDIter last)
 {
