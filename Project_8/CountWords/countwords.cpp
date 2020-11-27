4fb964136669e52d1ec0bf0f465aac6089bdc45e
lbunderwood <luke.b.underwood@gmail.com>
2020-11-27 12:26:26 -0900

    proj 8 - countwords nearly complete

--- Project_8/CountWords/countwords.cpp
+++ Project_8/CountWords/countwords.cpp
@@ -29,45 +29,81 @@
 
 // getFileName Function
 //
+// Takes filename string by reference and returns output via input string.
+// Returns false if the filename contains a space or 
+//  does not have a '.' separating the name and extension.
+// Returns true otherwise
+//
+// Preconditions : 
+//     filename must be a valid lvalue std::string
+// Exception Guaruntee :
+//     Strong guaruntee - uses commit/roll back
 bool getFileName(std::string& filename)
 {
-    // prompt user, and collect input
+    // use this string to ensure strong guaruntee
+    std::string tempString;
+
+    // prompt user and collect input
     std::cout << "Please enter the name of the file you would like to read from: ";
-    std::getline(std::cin, filename);
+    std::getline(std::cin, tempString);
     
     // We want to check that what has been entered is a valid filename.
     // since this varies from system to system, we will only have two requirements
     bool hasPeriod = false;
-    for(std::size_t i = 0; i < filename.size(); ++i)
+    for(std::size_t i = 0; i < tempString.size(); ++i)
     {
         // if filename contains a space, the input is invalid, so we return false
-        if(filename[i] == ' ')
+        if(tempString[i] == ' ')
         {
             return false;
         }
         // filename must contain a '.' to separate the name from the extension,
         // and as such it must not be the first or last character in the file name
-        else if(filename[i] == '.' && i != 0 && i != filename.size() - 1)
+        else if(tempString[i] == '.' && i != 0 && i != tempString.size() - 1)
         {
             hasPeriod = true;
         }
     }
 
+    // commit changes
+    filename = tempString;
+
     // return whether the filename is passably valid
     return hasPeriod;
 }
 
+
+
 // printErr function
 //
+// Prints an error message
+// Takes no parameters and returns void
+//
+// Preconditions : none
+// Exception Guaruntee : 
+//     Strong Guaruntee - no data to modify
 void printErr()
 {
-    std::cout << "Something went wrong!\n"
+    std::cout << "\nSomething went wrong!\n"
               << "Please check that the file exists and that you have entered the name correctly.\n"
-              << "Please Try again!\n";
+              << "Please Try again!\n\n";
 }
+
+
 
 // readFile function
 //
+// Takes an ifstream and a map<string, int>, both by reference.
+// Reads through the file, storing each distinct word in the map, and updating the number of times
+//  that the word appears in the file.
+// Returns void
+//
+// Preconditions : 
+//     Both parameters must be valid lvalues of their respective types.
+//     Words should be empty and inFile should be at the beginning of the file if the
+//      client code wishes to read the entire file and for words to only reflect that file's data.
+// Exception Guaruntee :
+//     Basic Guaruntee - function must constantly modify inFile and words throughout
 void readFile(std::ifstream& inFile, std::map<std::string, int>& words)
 {
     // keep grabbing new words until it fails to do so
@@ -88,8 +124,17 @@ void readFile(std::ifstream& inFile, std::map<std::string, int>& words)
 
 // printResults function
 //
+// Takes a map<string, int> by reference.
+// Tells the user how many words were stored in the map, then displays each alongside the
+//  number of times that they appeared.
+//
+// Preconditions :
 void printResults(const std::map<std::string, int>& words)
 {
+    // print distinct word count
+    std::cout << "There are " << words.size() << " distinct words in the file.\n\n";
+
+    // print each word and its frequency
     for(auto m : words)
     {
         std::cout << m.first << ": " << m.second << std::endl;
@@ -109,15 +154,21 @@ int main()
         // get the input, display an error message and try again if it fails
         if(!getFileName(filename)) ERROR
 
-        // open the filestream, display an error message and try again if it fails
+        // open the filestream, check for errors
         std::ifstream inFile(filename);
         if(!inFile) ERROR
 
+        // read in the words
         readFile(inFile, words);
 
+        // check if it stopped before reaching eof
         if(!inFile.eof()) ERROR
 
+        // spit out the results
         printResults(words);
+
+        // reset for the next file
+        words.clear();
     }
 
     return 0;
